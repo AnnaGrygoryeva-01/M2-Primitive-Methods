4dfb1803fc51615adf81c9f389ee5d4c052d5d98
Anna Grygoryeva <annagrygoryeva3101@gmail.com>
2026-01-14 23:08:50 +0000

    Complete task-4

--- index.js
+++ index.js
@@ -5,6 +5,18 @@
 // 3. Прийняти від користувача (за допомогою prompt) його ім’я і вивести його у модальне вікно великими літерами (‘alex’ -> ‘ALEX’)
 
 // 4. Написати функцію, яка приймає рядок тексту з датою у форматі “2021-22-09” і повертає її у вигляді “22.09.2021”
+const userDate = prompt("Please type the date (YYYY-DD-MM)");
+
+function formatDate(date) {
+  const parts = date.split("-");
+
+  const year = parts[0];
+  const day = parts[1];
+  const month = parts[2];
+  return day + "." + month + "." + year;
+}
+
+console.log(formatDate("2021-22-09"));
 
 // 5. Написати функцію, яка приймає два рядки, написані у різному регістрі та порівнює їх незалежно від регістру.
 // isEqual(‘pApA’, ‘papa’)  //true
